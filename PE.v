5173d45beeb4730aac1d9d443d8f92c43ab208e7
wcycw <wellischen58@gmail.com>
2018-05-08 15:25:11 -0400

    Add PE and bram

---
+++ PE.v
@@ -0,0 +1,95 @@
+module PE ( 
+	Clk, Resetn,
+	in0, in1, in2, in3,
+	out0, out1, out2, out3,
+	tf,
+	bypass_n
+);
+
+parameter WIDTH = 32;
+parameter SHIFT = 16;
+
+input					Clk;
+input 					Reset_n;
+
+input 		[WIDTH - 1 : 0]		in0;
+input 		[WIDTH - 1 : 0]		in1;
+input 		[WIDTH - 1 : 0]		in2;
+input 		[WIDTH - 1 : 0]		in3;
+
+output reg 	[WIDTH - 1 : 0]		out0;
+output reg 	[WIDTH - 1 : 0]		out1;
+output reg 	[WIDTH - 1 : 0]		out2;
+output reg 	[WIDTH - 1 : 0]		out3;
+
+input 		[WIDTH - 1 : 0]		tf;
+input 					bypass_n;
+
+
+reg		[WIDTH - 1 : 0]		mid0[0 : 3];
+reg		[WIDTH - 1 : 0]		mid1[0 : 3];
+reg		[2* WIDTH - 1 : 0]	mid11[0 : 1];
+
+
+
+always @ (posedge Clk)
+	begin
+	if (Reset_n == 1'b0)
+		begin
+		mid0[0] <= 0;
+		mid0[1] <= 0;
+		mid0[2] <= 0;
+		mid0[3] <= 0;
+		end
+	else 
+		begin
+		mid0[0] <= in0 + in1;
+		mid0[1] <= in0 - in1;
+		mid0[2] <= in2 + in3;
+		mid0[3] <= in2 - in3;
+		end
+	end
+
+always @ (posedge Clk)
+	begin
+	if (Reset_n == 1'b0)
+		begin
+		mid1[0] <= 0;
+		mid1[1] <= 0;
+		mid1[2] <= 0;
+		mid1[3] <= 0;
+		mid11[0] <= 0;
+		mid11[1] <= 0;
+		end
+	else 
+		begin
+		mid1[0] <= mid0[0];
+		mid1[1] <= mid0[2];
+		mid1[2] <= mid0[1];
+		mid1[3] <= mid0[3];
+		mid11[0] <= mid0[1] * tf;
+		mid11[1] <= mid0[3] * tf;
+		end
+	end
+
+
+always @ (posedge Clk)
+	begin
+	if (Reset_n == 1'b0)
+		begin
+		out0 <= 0;
+		out1 <= 0;
+		out2 <= 0;
+		out3 <= 0;
+		end
+	else 
+		begin
+		out0 <= mid1[0];
+		out1 <= mid1[1];
+		out2 <= (bypass_n) ? mid11[0][SHIFT + WIDTH - 1 : SHIFT] : mid1[2];
+		out3 <= (bypass_n) ? mid11[1][SHIFT + WIDTH - 1 : SHIFT] : mid1[3];
+		end
+	end
+
+
+endmodule
---
+++ bram.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:37:05 03/10/2017 
+// Design Name: 
+// Module Name:    bram_dual 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module bram_dual(
+	Clk, En, We, Addr_A,  DI_A,  DO_A
+    );
+parameter WIDTH = 32;
+
+input 		Clk;
+input 		En;
+input 		We;
+input 		[5 : 0]			Addr_A;
+
+input 		[WIDTH - 1 : 0]	DI_A;
+output 		[WIDTH - 1 : 0]	DO_A;
+
+
+reg 		[WIDTH - 1 : 0]		ram 	 [0 : 63];
+reg	 	[WIDTH - 1 : 0]	DO_A;
+
+
+always @(posedge Clk) 
+	begin
+	if (En)
+		begin
+		if (We)
+			ram[Addr_A] <= DI_A;
+		DO_A <= ram[Addr_A];
+		end 
+	end
+	
+
+
+endmodule
+
+
